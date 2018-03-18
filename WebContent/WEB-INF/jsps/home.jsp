74117f1fc3f9d69332f8f1a4c9b47391bcc49142
Aayush Neupane <aayush_Alex@yahoo.com>
2018-03-17 22:19:19 -0700

    connection pool
    enabled jhdi connection pool and using jstl to test that

--- WebContent/WEB-INF/jsps/home.jsp
+++ WebContent/WEB-INF/jsps/home.jsp
@@ -1,5 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql"%>
+
+<sql:query var="rs" dataSource="jdbc/dbPool">
+select id, email, name, text from offers
+</sql:query>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +14,15 @@
 <title>Insert title here</title>
 </head>
 <body>
-Hi there! 
+
+	<h2>Results</h2>
+
+	<c:forEach var="row" items="${rs.rows}">
+    Id ${row.id}<br>
+    Email ${row.email}<br>
+    Name ${row.name}<br>
+    Text ${row.text}<br>
+	</c:forEach>
+
 </body>
 </html>
--- src/aayush/neupane/me/web/controllers/OffersController.java
+++ src/aayush/neupane/me/web/controllers/OffersController.java
@@ -1,17 +1,40 @@
 package aayush.neupane.me.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import java.util.Map;
+//
+//import javax.servlet.http.HttpSession;
 
 @Controller
 public class OffersController {
 	
 	/*the home page. located at web-inf/jsps/. doesnt need full url 
-	 * because prefix and suffix are defined at spring-app-servlet.xml 
+	 * because prefix and suffix are defined at spring-app-servlet.xml.
+	 * 
+	 *  Using request to pass 'name' parameter instead of session. 
 	 */
 	
+//	@RequestMapping("/")
+//	public ModelAndView showHome(HttpSession session) {
+//		
+//		ModelAndView mv = new ModelAndView("home");
+//		Map<String, Object> model = mv.getModel();
+//		model.put("name", "Neupane");
+//		session.setAttribute("name", "Aayush");
+//		return mv;
+//	}
+	
+	/*
+	 * Using spring's model instead of creating own ModelAndView
+	 */
 	@RequestMapping("/")
-	public String showHome() {
+	public String showHome(Model model) {
+		
+		model.addAttribute("name", "<b>Aayush</b>");
 		return "home";
 	}
 
