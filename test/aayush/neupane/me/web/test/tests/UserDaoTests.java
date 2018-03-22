b6e653786be37d9b9da3df56142626772eae97cb
Aayush Neupane <aayush_Alex@yahoo.com>
2018-03-21 22:27:54 -0700

    jUnit

--- test/aayush/neupane/me/web/test/tests/UserDaoTests.java
+++ test/aayush/neupane/me/web/test/tests/UserDaoTests.java
@@ -1,5 +1,8 @@
 package aayush.neupane.me.web.test.tests;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -7,11 +10,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @ActiveProfiles("dev")
 @ContextConfiguration(locations= { 
-		"classpath:aayush.neupane.me.web.config/dao-context.xml",
-		"classpath:aayush.neupane.me.web.config/security-context.xml"
+		"classpath:aayush/neupane/me/web/config/dao-context.xml",
+		"classpath:aayush/neupane/me/web/config/security-context.xml"
 		 })
 
-@RunWith(SpringJUnit4ClassRunner.class);
+@RunWith(SpringJUnit4ClassRunner.class)
 public class UserDaoTests {
+	
+	@Test
+	public void testCreateuser() {
+		assertEquals("Dummy test", 1, 1);
+	}
 
 }
